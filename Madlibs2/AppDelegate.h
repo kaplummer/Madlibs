c84caf067017d740539a6aecc9b0b9f50603b2b1
Katherine Plummer <patriciaplummer@KATIE-PLUMMERs-MacBook-Pro.local>
2014-03-03 07:58:54 -0800

    Initial Commit

---
+++ Madlibs2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Madlibs2
+//
+//  Created by Katherine Plummer on 3/3/14.
+//  Copyright (c) 2014 Katherine Plummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Madlibs2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Madlibs2
+//
+//  Created by Katherine Plummer on 3/3/14.
+//  Copyright (c) 2014 Katherine Plummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
